0dea38b377242c3a880022aef3036e0a7f8ea332
Jonathan <jonathan213kim@gmail.com>
2020-06-20 14:33:45 -0700

    Updated with base Homework files and Server.js

--- Develop/server.js
+++ Develop/server.js
@@ -14,18 +14,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./html/index.html"));
-});
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./html/notes.html"));
+  res.sendFile(path.join(__dirname, "/html/notes.html"));
+  console.log("notes!");
 });
 
 app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./data/db.json"));
+  res.sendFile(path.join(__dirname, "/db.json"));
 });
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/html/index.html"));
+});
+app.get;
+app.post;
+app.delete;
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
