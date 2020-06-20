4ecdcca16153752423ba8c9643b933d09469467f
Jonathan <jonathan213kim@gmail.com>
2020-06-20 16:02:56 -0700

    Added save functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+const database = require("./db/db.json");
 const fs = require("fs");
 
 // Sets up the Express App
@@ -12,23 +13,35 @@ const PORT = process.env.PORT || 3003;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Basic route that sends the user first to the AJAX Page
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/html/notes.html"));
-  console.log("notes!");
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/db.json"));
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/html/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-app.get;
-app.post;
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function (err, data) {
+    const json = JSON.parse(data);
+    json.push(req.body);
+
+    fs.writeFile("./db/db.json", JSON.stringify(json), function (err) {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+    });
+  });
+  res.json(true);
+});
+
 app.delete;
 // Starts the server to begin listening
 // =============================================================
